a9083c18daa023fb851adcda503446e86c0e73cb
siddesha soldarahalli <siddu4825@gmail.com>
2023-10-03 17:49:11 +0530

    minor modifiications
    minor modifiications

---
+++ hello-stratospheric-main/README.md
@@ -0,0 +1,3 @@
+# Hello Stratospheric
+
+A Hello World app to showcase a continuous deployment pipeline with Spring Boot, CDK, and AWS.
---
+++ hello-stratospheric-main/src/main/java/dev/stratospheric/hello/IndexController.java
@@ -0,0 +1,14 @@
+package dev.stratospheric.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+  @GetMapping(path="/")
+  public String getIndex() {
+    return "index";
+  }
+
+}
